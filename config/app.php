eb06a6cd83458460245f2ae6275f87de3157a9f5
root <stephane.guillaume71@gmail.com>
2017-01-17 13:40:44 +0100

    install panel admin

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Backpack\Base\BaseServiceProvider::class,
+        Backpack\CRUD\CrudServiceProvider::class,
 
     ],
 
