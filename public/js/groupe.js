0fffe7f1979b183460946dd0a1b399c8c6c27106
Florian <cellier.florian@hotmail.fr>
2017-01-26 11:12:12 +0100

    Not ignore public upload files

--- public/js/groupe.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-	$( ".supprimer" ).click(function() {
-	 $(this).closest('tr').remove();
-	});
-});
-
-function modalModifGroupe(obj){
-	$('.modalGroupe').modal('show');
-}
